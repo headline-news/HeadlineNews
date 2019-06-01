9ec9eb00e10bd7c1a27b21bef35dabd3f0fded74
lvxm0 <31623469+lvxm0@users.noreply.github.com>
2019-06-01 11:30:05 +0800

    Revert "预览大图"
    This reverts commit 000dd7bc50acf06acc2947914265dfab4e6e03d8.

--- FeedsAPP/FeedsAPP/BigImg/BigImg.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-//预览大图
-@interface BigImg : NSObject
-
-+(void)BigImageWithImageView:(UIImageView *)currentImageview;
-
-+(void)BigImageWithImage:(UIImage *)image frame:(CGRect)pOldframe;
-@end
