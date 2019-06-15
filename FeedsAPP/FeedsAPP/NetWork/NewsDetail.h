4f7ae29fd43e44f6151382f60ea3b15c8fd153a9
liubq98 <43903410+liubq98@users.noreply.github.com>
2019-06-15 15:06:03 +0800

    解决详情页图片问题

---
+++ FeedsAPP/FeedsAPP/NetWork/NewsDetail.h
@@ -0,0 +1,14 @@
+//
+//  NewsDetail.h
+//  FeedsAPP
+//
+//  Created by student7 on 2019/6/1.
+//  Copyright © 2019 iosGroup. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface NewsDetail : UIViewController
+
+@property (nonatomic, strong) NSString *groupId;
+
+@end
