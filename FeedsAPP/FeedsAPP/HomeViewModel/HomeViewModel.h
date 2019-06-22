9c29f5c529e6c472ba02e2adca1dafe885e7d858
liubq98 <43903410+liubq98@users.noreply.github.com>
2019-06-22 11:03:54 +0800

    详情页

--- FeedsAPP/FeedsAPP/HomeViewModel/HomeViewModel.h
+++ FeedsAPP/FeedsAPP/HomeViewModel/HomeViewModel.h
@@ -13,6 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface HomeViewModel : NSObject
 
 @property (nonatomic,copy) NSString *title;
+@property (nonatomic,copy) NSString *prefix;
 @property (nonatomic,copy) NSString *groupId;
 @property (nonatomic,copy) NSArray *image;
 
